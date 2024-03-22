35305860bc1c41411ee6780cac45a7dfac5cef71
bluepenit <blupeen0613.it@gmail.com>
2024-03-22 15:17:54 -0700

    removed unnecessary code of background.js

--- AstroResumeAI_Frontend/AstroResumeAI_Extension/background.js
+++ AstroResumeAI_Frontend/AstroResumeAI_Extension/background.js
@@ -1,13 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Dynamic Job Scraper installed.');
-
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'getHostname') {
-      chrome.tabs.sendMessage(message.tabId, { action: "getHostname" }, (response) => {
-        sendResponse(response);
-      });
-      return true; // Needed to indicate that sendResponse will be called asynchronously
-    }
-  });
 });
 
