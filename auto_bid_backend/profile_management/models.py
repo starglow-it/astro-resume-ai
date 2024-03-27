e265d30b00ff9b0dc5f9a1afe02152403f2d73ee
jameskai-starglow <155010041+jameskai-starglow@users.noreply.github.com>
2024-03-27 13:43:47 -0700

    update migrations

--- auto_bid_backend/profile_management/models.py
+++ auto_bid_backend/profile_management/models.py
@@ -9,12 +9,12 @@ class Profile(models.Model):
     recent_role = models.CharField(max_length=255)
     phone = models.CharField(max_length=255, blank=True)  # Assuming this can be optional as well
     location = models.CharField(max_length=255, blank=True, null=True)  # Allow null and blank
-    summary = models.TextField(blank=True)  # Assuming this can be optional as well
+    summary = models.TextField(blank=True, null=True)  # Assuming this can be optional as well
     skills = models.JSONField(default=list)  # Storing as a list in a JSON field, assumed optional
     work_authorization = models.CharField(max_length=255, blank=True)  # Assuming this can be optional as well
-    website = models.CharField(max_length=255, blank=True)
-    linkedin = models.CharField(max_length=255, blank=True)
-    github = models.CharField(max_length=255, blank=True)
+    website = models.CharField(max_length=255, blank=True, null=True)
+    linkedin = models.CharField(max_length=255, blank=True, null=True)
+    github = models.CharField(max_length=255, blank=True, null=True)
 
 class Education(models.Model):
     profile = models.ForeignKey(Profile, related_name='education', on_delete=models.CASCADE)
